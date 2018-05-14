f99ee504a92dab91b7a59fe8704e9dc10529bc62
mac <a3125045@gmail.com>
2018-05-14 11:40:46 +0800

    Revert "修改index和一堆static的東C"
    This reverts commit eb164fa8cec5e5ab942f3ef87345c1cf77eee6f3.

--- Recognition_Project/main.py
+++ Recognition_Project/main.py
@@ -17,7 +17,7 @@ def connect_to_db() :
 @app.route('/')
 @app.route('/index/')
 def index() :
-	return render_template('index.html')
+	return 'index page'
 
 # Route: '/register'
 @app.route('/register/', methods=['GET', 'POST']) 
