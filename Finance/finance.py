8fd40c639e82be340f3a833b93c62a3942a119e8
kobechao <chaokobe@gmail.com>
2018-05-14 11:53:30 +0800

    Finance Node Testing

--- Finance/finance.py
+++ Finance/finance.py
@@ -10,13 +10,16 @@ def main() :
 		print( 'Press Ctrl+C To Stop Demo\n' )
 		userName = str( input("Please Type User's Name To Get Data: ") )
 		contractAddress, contractABI, userToken = getDataFromGovernment( userName )
-		# print( contractAddress )
-		# print( contractABI )
-		# print( userToken )
-		getContract( contractAddress, contractABI )
+		if all( [contractAddress, contractABI, userToken] ) :
+			ID_Recognition_Contract = getContract( contractAddress, contractABI )
+			test( ID_Recognition_Contract )
+		else:
+			print( 'No Such User' )
+
 		time.sleep(0.5)
 	
 
+# =====================================
 def getDataFromGovernment( userName ) :
 	
 	link = 'http://localhost:5000/contract'
@@ -31,26 +34,33 @@ def getDataFromGovernment( userName ) :
 	return res['contractAddress'], res['contractABI'], res['userToken']
 
 
-
+# ===============================================
 def getContract( contractAddress, contractABI ) :
 
 	from web3 import Web3, HTTPProvider, contract
-	from solc import compile_source, compile_files, link_code
+	from web3.contract import ConciseContract
+	import json
 	
 	web3 = Web3( HTTPProvider( 'http://localhost:8545' ) )
 	eth = web3.eth
 	assert web3.isConnected()
 	assert eth.accounts
+	contractABI = eval(contractABI) 
 
-	ID_Recognition_Contract = eth.contract( address=contractAddress, )
-	print( ID_Recognition_Contract.address )
-	print( ID_Recognition_Contract.abi )
-	pass
+	ID_Recognition_Contract = eth.contract( address=contractAddress, abi=contractABI, ContractFactoryClass=ConciseContract )
+	# _tx = ID_Recognition_Contract.getUserRegisterTable( "kobe" )
 
+	return ID_Recognition_Contract
 
+# ================================
 def signal_handler(signal, frame):
     print ('\nExit By Keyboad! Bye!' )
     sys.exit(0)
+
+# ==================================
+def test( ID_Recognition_Contract) :
+	for testName in ['kobe','tina', 'cindy', 'hao', 'lulu'] :
+		print( testName + '\t' + str(ID_Recognition_Contract.getUserRegisterTable( testName ) ))
 
 
 if __name__ == '__main__':
